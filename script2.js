a396c10a2e6ac744de02ec246ec2f69778280a95
albinbn <albinbn@hotmail.com>
2016-02-08 20:58:20 +0100

    gjorde fel
    del

--- script2.js
+++ script2.js
@@ -12,11 +12,11 @@ var x = d3.scale.ordinal()
 var y = d3.scale.linear()
     .range([height, 0]);
 
-var xAxis = d3.svg1.axis()
+var xAxis = d3.svg.axis()
     .scale(x)
     .orient("bottom");
 
-var yAxis = d3.svg1.axis()
+var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left")
     .tickFormat(function(d) { return d + "%"; });
