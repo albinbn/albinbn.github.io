28ac0691b421cd26fcd7187aed8e897ad04b6532
albinbn <albinbn@hotmail.com>
2016-02-08 21:06:08 +0100

    W+H
    whw

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var margin = {top: 20, right: 20, bottom: 50, left: 60},
-    width = 900 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    width = 1000 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
     
 var formatPercent = d3.format(".0%");    
 
