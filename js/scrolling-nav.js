3dca3b8e34b7c723219e7ed2165e7e0b163eaf0c
albinbn <albinbn@hotmail.com>
2016-04-20 15:05:16 +0200

    sdf
    afd

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -30,7 +30,7 @@ $.get("http://ipinfo.io", function (response) {
 
 var OSName="Unknown OS";
 if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
-if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+if (navigator.appVersion.indexOf("Mac")!=-1) OSName="Mac OS";
 if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
 if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
 
